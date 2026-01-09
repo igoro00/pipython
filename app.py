3a96799b9cf9265dcd76250c64b9c4e3fd6a4c13
igoro00 <igor.ordecha@gmail.com>
2026-01-09 20:07:28 +0100

    add form

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, DecimalField
+from wtforms.validators import DataRequired
 
 # SQLite URI compatible
 WIN = sys.platform.startswith('win')
@@ -25,15 +28,31 @@ class Record(db.Model):
     f2 = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(100), nullable=False)
 
+class NewRecordForm(FlaskForm):
+    f1 = DecimalField('f1', validators=[DataRequired()])
+    f2 = DecimalField('f2', validators=[DataRequired()])
+    category = StringField('Category', validators=[DataRequired()])
+    submit = SubmitField('Add Record')
+
 @app.route('/')
 def index():
     records = Record.query.all()
     return render_template('index.html', records=records)
 
-@app.route('/add', methods=['GET'])
-def add():
-    return render_template('add.html')
-
+@app.route('/add', methods=['GET','POST'])
+def add_record():
+    form = NewRecordForm()
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        f1 = form.f1.data
+        f2 = form.f2.data
+        category = form.category.data
+        record = Record(f1=f1, f2=f2, category=category)
+        print(record)
+        db.session.add(record)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form), 400 if form.errors else 200
 
 if __name__ == '__main__':
     with app.app_context():
