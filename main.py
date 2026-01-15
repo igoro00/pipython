079eeb508322f4db8326e760b85a0ad5f354b57a
igoro00 <igor.ordecha@gmail.com>
2026-01-15 16:11:36 +0100

    moving to uv, added delete and some api routes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import sys
-from flask import Flask, render_template, redirect, url_for
+import json
+from dataclasses import dataclass
+from flask import Flask, render_template, redirect, url_for, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, DecimalField
 from wtforms.validators import DataRequired
+
 
 # SQLite URI compatible
 WIN = sys.platform.startswith('win')
@@ -22,7 +25,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+@dataclass
 class Record(db.Model):
+    id: int
+    f1: float
+    f2: float
+    category: str
+
     id = db.Column(db.Integer, primary_key=True)
     f1 = db.Column(db.Float, nullable=False)
     f2 = db.Column(db.Float, nullable=False)
@@ -42,7 +51,6 @@ def index():
 @app.route('/add', methods=['GET','POST'])
 def add_record():
     form = NewRecordForm()
-    print(form.validate_on_submit())
     if form.validate_on_submit():
         f1 = form.f1.data
         f2 = form.f2.data
@@ -53,6 +61,28 @@ def add_record():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('add.html', form=form), 400 if form.errors else 200
+
+@app.route('/delete/<int:id>', methods=["POST"])
+def delete_record(id):
+    record = Record.query.get_or_404(id)
+    db.session.delete(record)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    records = Record.query.all()
+    return jsonify(records)
+
+@app.route('/api/data', methods=['POST'])
+def add_data():
+    data = request.get_json()
+    record = Record(*data)
+    db.session.add(record)
+    db.session.commit()
+    return jsonify(record)
+
+
 
 if __name__ == '__main__':
     with app.app_context():
